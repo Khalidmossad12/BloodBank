beeefce163c0a951e66ee8a52483824646f47308
khalid <khalid.mossad123456@gmail.com>
2021-11-16 18:47:02 +0200

    initial commit

---
+++ src/app/dash-request/dash-request.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dash-request',
+  templateUrl: './dash-request.component.html',
+  styleUrls: ['./dash-request.component.css']
+})
+export class DashRequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
