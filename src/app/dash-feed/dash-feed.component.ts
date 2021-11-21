fa5576a33881473699b2e28be9d314394df85cee
khalid <khalid.mossad123456@gmail.com>
2021-11-21 14:15:02 +0200

    first

--- src/app/dash-feed/dash-feed.component.ts
+++ src/app/dash-feed/dash-feed.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-dash-feed',
   templateUrl: './dash-feed.component.html',
@@ -10,6 +10,19 @@ export class DashFeedComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+   
   }
-
+  Toggle(){
+    $('button').next('.nav-collapse').slideToggle(200);
+  }
+  Comment_And_Close(){
+    $('.list_of_comments').fadeToggle(200);
+  }
+  Share_And_Close(){
+    $('.share').fadeToggle(200);
+  }
+  likes_And_Close(){
+    $('.likes').fadeToggle(200);
+    // $('#emoj').html($('.likes .emojs').html());
+  }
 }
